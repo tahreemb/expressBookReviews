1a8e8cc048172848d3c9e8be0f0717b9e5bb31ba
tahreemb <tahreembhatti19@gmail.com>
2023-10-04 20:24:54 -0400

    changes made from theia lab environment

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -51,8 +51,24 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+            filtered_book['reviews'][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+//code to delete a book review here
 });
 
 module.exports.authenticated = regd_users;
